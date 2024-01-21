9b6b8116bd6f0431242d46e309cb4dadce0e6412
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2024-01-21 15:25:34 +0200

    fishing adding tick nontick and delete

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -13,10 +13,19 @@ const Todo = () => {
     const add = () => {
         setTodos([...todos, {no:count++, text:inputRef.current.value, display:""}]);
         inputRef.current.value = "";
+        localStorage.setItem("todos_count", count)
     }
 
     useEffect(()=>{
-        console.log(todos);
+        setTodos(JSON.parse(localStorage.getItem("todos")));
+        count = localStorage.getItem("todos_count");
+    }, [])
+
+    useEffect(()=>{
+        setTimeout(() => {
+            console.log(todos);
+            localStorage.setItem("todos",JSON.stringify(todos));
+        }, 100)
     }, [todos])
 
 
@@ -29,7 +38,7 @@ const Todo = () => {
         </div>
         <div className="todo-list">
             {todos.map((item,index)=>{
-                return <TodoItems key={index} no={item.no} display={item.display} text={item.text}/>
+                return <TodoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text}/>
             })}
         </div>
     </div>
